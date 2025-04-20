a08368faf6890064940b09f3bfc5213efc3ff4ca
Adrian Liechti <adrian@monobox.ch>
2025-04-20 14:23:16 +0200

    support prompt templates

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -11,6 +11,7 @@ import (
 	"github.com/adrianliechti/wingman-cli/pkg/tool"
 
 	wingman "github.com/adrianliechti/wingman/pkg/client"
+	"github.com/adrianliechti/wingman/pkg/template"
 )
 
 func ParsePrompt() (string, error) {
@@ -22,10 +23,23 @@ func ParsePrompt() (string, error) {
 		data, err := os.ReadFile(name)
 
 		if err != nil {
-			continue
+			return "", err
 		}
 
 		prompt := strings.TrimSpace(string(data))
+
+		tmpl, err := template.NewTemplate(prompt)
+
+		if err != nil {
+			return "", err
+		}
+
+		prompt, err = tmpl.Execute(nil)
+
+		if err != nil {
+			return "", err
+		}
+
 		return prompt, nil
 	}
 
